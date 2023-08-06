bce79f58c3c28e28700652de94eda002f87351ac
Ricardo Soares Ribolli <rsoaresribolli@gmail.com>
2023-08-05 20:12:37 -0400

    Initialize repo

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    roots: ['<rootDir>/test'],
+    coverageDirectory: 'coverage',
+    collectCoverageFrom: [
+      '<rootDir>/src/**/*.ts',
+      '!**/test/**',
+      '!**/config/**'
+    ],
+    transform: {
+      '.+\\.ts$': 'ts-jest'
+    },
+    moduleNameMapper: {
+      '^@/(.*)$': '<rootDir>/src/$1',
+      '^@test/(.*)$': '<rootDir>/test/$1'
+    },
+    // setupFiles: ['dotenv/config']
+  }
+  
